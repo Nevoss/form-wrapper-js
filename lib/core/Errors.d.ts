d632b07e4c3890d5406252bdf51c56b29ee78f09
Nevo <nevos12@gmail.com>
2018-11-20 18:36:01 +0200

    Changing the compile options of ts

---
+++ lib/core/Errors.d.ts
@@ -0,0 +1,63 @@
+import { ErrorsStack } from "../types";
+export declare class Errors {
+    /**
+     * Errors stack, holds all the form errors
+     */
+    $errors: ErrorsStack;
+    /**
+     * Construct the Errors class with errors
+     *
+     * @param errors
+     */
+    constructor(errors?: ErrorsStack);
+    /**
+     * Record errors to the ErrorsStack
+     *
+     * @param errors
+     */
+    record(errors: ErrorsStack): Errors;
+    /**
+     * Append errors to the ErrorsStack
+     *
+     * @param errors
+     */
+    append(errors: ErrorsStack): Errors;
+    /**
+     * checks if fieldKey exists in the ErrorsStack
+     *
+     * @param fieldKey
+     */
+    has(fieldKey: string): boolean;
+    /**
+     * Returns array of errors for specific field
+     *
+     * @param fieldKey
+     * @param defaultValue
+     */
+    get(fieldKey: string, defaultValue?: any): string[];
+    /**
+     * returns first error of specific field key
+     *
+     * @param fieldKey
+     * @param defaultValue
+     */
+    getFirst<T>(fieldKey: string, defaultValue?: T): T | string;
+    /**
+     * Returns all the ErrorsStack
+     */
+    all(): ErrorsStack;
+    /**
+     * delete a key from ErrorsStack
+     *
+     * @param fieldKey
+     */
+    delete(fieldKey: string): Errors;
+    /**
+     * check if there is any error in the ErrorsStack
+     */
+    any(): boolean;
+    /**
+     * Clear the ErrorsStack object
+     */
+    clear(): Errors;
+}
---
+++ lib/types.d.ts
@@ -0,0 +1,88 @@
+import { Form } from "./core/Form";
+/**
+ * Field object that passes the PassesFunction and MessageFunction,
+ * it used in the Validator class.
+ */
+export interface Field {
+    key: string;
+    label: string;
+    value: any;
+}
+/**
+ * Passes function is a prop in Rule Object
+ */
+export interface PassesFunction {
+    (field: Field, form: Form): boolean;
+}
+/**
+ * Message function is a prop in the Rule Object
+ */
+export interface MessageFunction {
+    (field: Field, form: Form): string;
+}
+/**
+ * The Rule interface in responsible to check value of field is valid,
+ * and send error message if not.
+ */
+export interface Rule {
+    passes: PassesFunction;
+    message: MessageFunction;
+}
+/**
+ * Holds all the rules.
+ * each key is the field which hold an array of rules
+ */
+export interface RulesStack {
+    [fieldKey: string]: Rule[];
+}
+/**
+ * Errors Stack must be a field key with an array of strings
+ */
+export interface ErrorsStack {
+    [fieldKey: string]: string[];
+}
+/**
+ * options that related to successful submission of the form
+ */
+export interface SuccessfulSubmissionOptions {
+    /**
+     * Clear errors after successful submission
+     */
+    clearErrors?: boolean;
+    /**
+     * Set the data to $originalData after successful submission
+     */
+    resetData?: boolean;
+}
+/**
+ * options that related to validation
+ */
+export interface ValidationOptions {
+    /**
+     * when validation field with a chain of validation rules if this option assigned as true,
+     * for this specific field it is stopping to validate the other rules
+     */
+    stopAfterFirstRuleFailed?: boolean;
+    /**
+     * should or not should validate the form on submission
+     */
+    onSubmission?: boolean;
+    /***
+     * Default message for errors
+     */
+    defaultMessage?: MessageFunction;
+}
+/**
+ * Form general Options
+ */
+export interface Options {
+    successfulSubmission?: SuccessfulSubmissionOptions;
+    validation?: ValidationOptions;
+}
+/**
+ * Submit callback interface,
+ * the function the should pass to submit method in Form class
+ */
+export interface SubmitCallback {
+    (form: Form): Promise<any>;
+}
