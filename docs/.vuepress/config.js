4c112beaf42c69d022c1ce017049876cb25e52a0
Nevo <nevos12@gmail.com>
2019-01-19 10:20:04 +0200

    Adding vue-press and starting writing the docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Form Wrapper JS',
+  description: 'JS abstraction for forms',
+  themeConfig: {
+    repo: 'Nevoss/form-wrapper-js',
+    editLinks: true,
+    nav: [
+      { text: 'Guide', link: '/guide/' },
+    ],
+    sidebar: [
+      ['/guide/getting-started', 'Getting started']
+    ],
+  }
+}
