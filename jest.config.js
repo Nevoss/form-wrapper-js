1d21f68dec8d0007af39b734585f2aa91a9c37db
Nevo <nevos12@gmail.com>
2019-06-02 17:40:37 +0300

    wip

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,4 @@ module.exports = {
   moduleNameMapper: {
     '~(.*)$': '<rootDir>/src/$1',
   },
-  setupFiles: ['<rootDir>/tests/jest.setup.ts'],
-  // setupTestFrameworkScriptFile: 'jest-extended',
 }
--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,15 @@
 import typescript from 'rollup-plugin-typescript2';
+import sourceMaps from 'rollup-plugin-sourcemaps'
 import pkg from './package.json'
 
 export default {
   input: './src/index.ts',
   output: [
+    { file: pkg.main, name: 'formWrapperJs', format: 'umd', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   plugins: [
-    typescript()
+    typescript({ useTsconfigDeclarationDir: true }),
+    sourceMaps()
   ]
 }
