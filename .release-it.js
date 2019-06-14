778a4ca48364e664ee63ead23c402dd41d0381f2
Nevo <nevos12@gmail.com>
2019-06-14 10:01:06 +0300

    chore: loading .env in the release process

--- .release-it.js
+++ .release-it.js
@@ -1,4 +1,10 @@
+// Load env files to the process
+require('dotenv').config()
+
 module.exports = {
+  hooks: {
+    'before:init': ['npm run test:prod'],
+  },
   git: {
     tagName: 'v${version}',
     changelog: false,
