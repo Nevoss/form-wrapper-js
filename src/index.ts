31cfa9fe3b743bd66842c2648bdf65c131753ff4
Nevo Golan <nevos12@gmail.com>
2019-06-21 13:37:06 +0300

    fix: expose RuleValidationError error class. (#58)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Form } from './core/Form'
 import { FormCollection } from './core/FormCollection'
 import { ConditionalRules } from './core/ConditionalRules'
+import { RuleValidationError } from './errors/RuleValidationError'
 
 const createForm = Form.create
 const createFormCollection = FormCollection.create
@@ -10,6 +11,7 @@ export {
   Form,
   FormCollection,
   ConditionalRules,
+  RuleValidationError,
   createForm,
   createFormCollection,
   applyRulesIf,
